681c0b5d4b515771e2a70b5fef882473622f15c2
Ramji Gupta <ramjigupta8310@gmail.com>
2025-02-13 16:36:53 +0530

    updating vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir:'dist' // This will create the production build in the 'dist' folder
-  },
-  server: {
-    historyApiFallback: true,  // Enable history fallback (for single-page apps)
-  },
-})
+  base: '/',
+});
